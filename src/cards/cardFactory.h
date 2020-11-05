7351a2a13bf594ac67478f083bd81e432d373d8b
iamnotrodger <rodger.retanal24@gmail.com>
2020-11-04 19:28:49 -0500

    implemented Singleton pattern on CardFactory

--- src/cards/cardFactory.h
+++ src/cards/cardFactory.h
@@ -6,9 +6,13 @@ namespace cards
     class CardFactory
     {
     public:
-        CardFactory();
         //returns a pointer to the only instance of card factory
         static CardFactory *getFactory();
         Deck getDeck();
+
+    private:
+        static CardFactory *const cardFactory;
+        CardFactory();
+        ~CardFactory();
     };
 } // namespace cards
