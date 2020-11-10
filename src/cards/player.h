b526217a529ab274df75f4281f26ea02e63536a0
iamnotrodger <rodger.retanal24@gmail.com>
2020-11-10 17:19:52 -0500

    small overall changes to the project

--- src/cards/player.h
+++ src/cards/player.h
@@ -10,6 +10,7 @@ namespace cards
     private:
         std::string name;
         int coins;
+        int numChain;
         std::vector<Chain_Base *> chains;
 
     public:
@@ -34,8 +35,10 @@ namespace cards
         //adds an empty third chain to the player for three coins. The functions reduces the coint count for the player by two. if the player does not have enough coins then an exception NotEnoughCoins is thrown
         void buyThirdChain();
         //prints the top card of the player's hand (argument false) or all of the plaeyr's hand (with argument true) to the supplied ostream
-        void printHand(std::ostream &, bool);
         friend std::ostream &operator<<(std::ostream &, const Player &);
+
+    protected:
+        void printHand(std::ostream &, bool);
     };
 
     inline std::ostream &operator<<(std::ostream &os, const Player &p)
