b4445e182e5ac223111b0dfba538a8a014d12bdb
iamnotrodger <rodger.retanal24@gmail.com>
2020-11-11 11:59:34 -0500

    Small tweak to IllegalType.h

--- src/cards/illegalType.h
+++ src/cards/illegalType.h
@@ -1,12 +1,14 @@
+#pragma once
 #include <exception>
 
 namespace cards
 {
     class IllegalType : public std::exception
     {
+    public:
         virtual const char *what() const throw()
         {
-            return "You are using an Illegal Type of card";
+            return "Error: You are using an Illegal Type of card";
         }
     };
 } // namespace cards
