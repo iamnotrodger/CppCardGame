778360b0eeb093eed984e377eb70f563ced1ad2d
unknown <patpat12345@hotmail.com>
2020-11-11 11:52:14 -0500

    Implemented Player, not tested yet

--- src/cards/hand.h
+++ src/cards/hand.h
@@ -22,6 +22,7 @@ namespace cards
         //returns but does not remove the top card form the player's hand
         Card *top();
         Card *operator[](int);
+        int getSize() {return hand.size()};
         //returns and removes the card from the players hands at the given index
         friend std::ostream &operator<<(std::ostream &, const Hand &);
     };
---
+++ src/cards/player.cpp
@@ -0,0 +1,41 @@
+#include "player.h"
+
+namespace cards
+{
+    Player &Player::operator+=(int addCoins) {
+        coins += addCoins;
+        return *this;
+    }
+
+    int Player::getMaxNumChains() {
+        return numChain;
+    }
+
+    Chain_Base &Player::operator[](int i) {
+        return *chains[i];
+    }
+
+    void Player::buyThirdChain() {
+        if (coins < 3) {
+            throw "NotEnoughCoins";
+        }
+
+        if (numChain != 3) {
+            coins -= 3;
+            numChain = 3;
+        }
+        
+    }
+
+    void Player::printHand(bool show) {
+        if (!show) {
+            std::cout << hand.top()->getName() << std::endl;
+        }
+
+        else {
+            for (int i = 0; i < hand.getSize(); i++) {
+                std::cout << hand[i]->getName() << std::endl;
+            }
+        }
+    }
+} 
