a9e64cd7c05dcb077f00b703035fc28045aced58
unknown <patpat12345@hotmail.com>
2020-11-10 21:10:31 -0500

    Added descardPile implementation

---
+++ src/cards/tradeArea.cpp
@@ -0,0 +1,24 @@
+#include "tradeArea.h"
+
+namespace cards
+{
+    TradeArea::TradeArea() {
+
+    }
+
+    TradeArea::~TradeArea() {
+
+    }
+
+    TradeArea &TradeArea::operator+=(Card * card) {
+
+    }
+
+    bool TradeArea::legal(Card * card) {
+
+    }
+
+    Card * TradeArea::trade(std::string bean) {
+
+    }
+} 
